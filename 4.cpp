ee64821b0d72ec2eaa34635a17bef055e559a44a
Kumar Gaurav <153125396+iamgaurva@users.noreply.github.com>
2026-07-25 23:23:41 +0530

    Create 4.cpp

---
+++ 4.cpp
@@ -0,0 +1,13 @@
+// Input two integers and print their sum.
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    int a, b;
+    cout<< "Enter a & b: ";
+    cin >> a >> b;
+    // cout<< "Enter b: ";
+    // cin >> b;
+    cout <<"Sum of a + b is: " <<a + b; 
+}
