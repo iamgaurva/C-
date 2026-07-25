93f637393f95bab1af35e32eec4810f2112831ae
Kumar Gaurav <153125396+iamgaurva@users.noreply.github.com>
2026-07-26 00:22:16 +0530

    Create _13.cpp

---
+++ _13.cpp
@@ -0,0 +1,13 @@
+//Find the average of three numbers.
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b, c;
+    float average;
+    cin >> a >> b >> c;
+    average = (a+b+c)/3;
+    cout << "average is: "<< average;
+    return 0;
+}
