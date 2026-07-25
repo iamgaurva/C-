096592b4d89d494d480acf1dcbed4610fef7f5ef
Kumar Gaurav <153125396+iamgaurva@users.noreply.github.com>
2026-07-25 23:10:11 +0530

    Create name.cpp

---
+++ name.cpp
@@ -0,0 +1,12 @@
+// Print your name, age, and city.
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    string name = "Kumar Gaurav" ;
+    int age = 21 ;
+    string city = "Noida" ;
+    
+    cout << name <<endl << age <<endl << city ;
+}
