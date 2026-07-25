2c8f881b163ef0d467d648c58ef09dc65dce040c
Kumar Gaurav <153125396+iamgaurva@users.noreply.github.com>
2026-07-25 23:31:20 +0530

    Create 5.cpp

---
+++ 5.cpp
@@ -0,0 +1,15 @@
+//Input two integers and print sum, difference, product, quotient, and remainder.
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    int a, b;
+    cout<< "Enter a & b: ";
+    cin >> a >> b;
+    cout <<"Sum of a + b is: " <<a + b << endl; 
+    cout <<"difference of a - b is: " <<a - b << endl;; 
+    cout <<"product of a * b is: " <<a * b << endl;; 
+    cout <<"quotient of a / b is: " <<a / b << endl;; 
+    cout <<"reminder of a % b is: " <<a % b << endl;; 
+}
