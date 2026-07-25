664c9f60af59a991d075cdd0a505d719eacc4c46
Kumar Gaurav <153125396+iamgaurva@users.noreply.github.com>
2026-07-26 01:01:59 +0530

    Create _16.cpp

---
+++ _16.cpp
@@ -0,0 +1,17 @@
+//Find the square and cube of a number.
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, square, cube;
+    cout<< "enter a number: ";
+    cin>> a;
+    
+    square = a*a;
+    cube = a*a*a;
+    cout << square << endl;
+    cout << cube;
+    
+    return 0;
+}
