8991f24ac4387137d4bd12c78a0a7b2a3ea6b630
Kumar Gaurav <153125396+iamgaurva@users.noreply.github.com>
2026-07-26 06:21:34 +0530

    Create _30.cpp

---
+++ _30.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+  int a, b;
+  char op;
+  cin >> a;
+  cin >> op ;
+  cin >> b;
+  
+  switch(op){
+      case'+':
+      cout << "sum: " << a +b;
+      break;
+      case'-':
+      cout << "diff: " << a - b;
+      break;
+      case'*':
+      cout << "product: " << a *b;
+      break;
+      case'/':
+      if (b != 0)
+        cout << "div = " << a /b;
+        else
+            cout << "Error!";
+        break;
+  }
+  return 0;
+}
