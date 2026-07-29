5bde43365f4d8c44cb4f34e6a78f87e08ac61577
Kumar Gaurav <153125396+iamgaurva@users.noreply.github.com>
2026-07-29 09:43:24 +0530

    Create _40.cpp

---
+++ _40.cpp
@@ -0,0 +1,15 @@
+//Print all odd numbers up to N.
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	int n;
+	cin >> n;
+
+	for (int i = 1; i <= n; i+=2){
+			cout << i << " ";
+	}
+
+	return 0;
+}
