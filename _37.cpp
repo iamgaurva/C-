57877ee7914f083864eb958b1ae6fc66a2a12860
Kumar Gaurav <153125396+iamgaurva@users.noreply.github.com>
2026-07-29 09:30:23 +0530

    Create _37.cpp

---
+++ _37.cpp
@@ -0,0 +1,15 @@
+//Print numbers from 1 to N.
+
+#include <iostream>
+using namespace std;
+int main(){
+    
+    int n;
+    cin >> n;
+    for(int i = 1; i<=n; i++){
+        cout <<i;
+    }
+    
+    
+    return 0;
+}
