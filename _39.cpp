a44aab5dc823a406160bdc4ac7c2f87b3c61f2ce
Kumar Gaurav <153125396+iamgaurva@users.noreply.github.com>
2026-07-29 09:41:48 +0530

    Create _39.cpp

---
+++ _39.cpp
@@ -0,0 +1,34 @@
+//Print all even numbers up to N.
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	int n;
+	cin >> n;
+
+	for (int i = 0; i <= n; i++) {
+		if(i % 2 == 0) {
+			cout << i << " ";
+		}
+	}
+
+	return 0;
+}
+
+
+//more efficent one:
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	int n;
+	cin >> n;
+
+	for (int i = 0; i <= n; i+=2){
+			cout << i << " ";
+	}
+
+	return 0;
+}
