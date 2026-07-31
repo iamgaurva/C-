7b5bdba5a54e13aec9e40437b288edda8c3099c0
Kumar Gaurav <153125396+iamgaurva@users.noreply.github.com>
2026-07-31 07:02:46 +0530

    Create _48.cpp

---
+++ _48.cpp
@@ -0,0 +1,21 @@
+//Find the sum of digit.
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    int sum = 0;
+
+    while (n != 0) {
+        int digit = n % 10;
+        sum = sum + digit;
+        n = n / 10;
+    }
+
+    cout << sum;
+
+    return 0;
+}
