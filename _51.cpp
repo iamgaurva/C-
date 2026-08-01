3be7c7f1d7a582617c0ba8714881f6d331150150
Kumar Gaurav <153125396+iamgaurva@users.noreply.github.com>
2026-08-02 01:40:41 +0530

    Update _51.cpp

--- _51.cpp
+++ _51.cpp
@@ -1,2 +1,23 @@
 //Armstrong number
 
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n = 153;
+    int sum = 0;
+    int temp = n;
+
+    while (temp != 0) {
+        int digit = temp % 10;
+        sum = sum + (digit * digit * digit);
+        temp = temp / 10;
+    }
+
+    if (sum == n)
+        cout << "Armstrong Number";
+    else
+        cout << "Not Armstrong Number";
+
+    return 0;
+}
