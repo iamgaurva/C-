6fa2ebe15644aea484b87a6218f1439ac9e976ab
Kumar Gaurav <153125396+iamgaurva@users.noreply.github.com>
2026-08-03 20:53:19 +0530

    Update _57.cpp

--- _57.cpp
+++ _57.cpp
@@ -1,1 +1,19 @@
-//
+// A
+// AB
+// ABC
+// ABCD
+
+#include <iostream>
+using namespace std;
+int main(){
+    char n;
+    cin >> n;
+    for(char i='A';i<=n;i++){
+        for(char j='A'; j<=i;j++){
+            cout<<j;
+        }
+        cout<<endl;
+    }
+
+    return 0;
+}
