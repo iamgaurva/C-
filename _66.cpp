15a5d1ec284437f3e44580772ce65e822563d938
Kumar Gaurav <153125396+iamgaurva@users.noreply.github.com>
2026-08-06 10:18:39 +0530

    Create _66.cpp

---
+++ _66.cpp
@@ -0,0 +1,24 @@
+// lcm
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b;
+    cin >> a >> b;
+
+    int x = a, y = b;
+
+    while (y != 0) {
+        int rem = x % y;
+        x = y;
+        y = rem;
+    }
+
+    int gcd = x;
+    int lcm = (a * b) / gcd;
+
+    cout << "LCM = " << lcm;
+
+    return 0;
+}
